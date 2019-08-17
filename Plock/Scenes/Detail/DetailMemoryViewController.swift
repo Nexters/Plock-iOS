ad9ca4586989e7da2940cfcede2e7f25936769b6
Zedd0202 <ch41150@hanmail.net>
2019-08-17 22:01:21 +0900

    detail view controller 생성.

---
+++ Plock/Scenes/Detail/DetailMemoryViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailMemoryViewController.swift
+//  Plock
+//
+//  Created by Zedd on 17/08/2019.
+//  Copyright © 2019 Zedd. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DetailMemoryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var memory: [Memory] = [Memory(), Memory(), Memory()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupNavigation()
+        self.setupCollectionView()
+    }
+    
+    func setupNavigation() {
+       // let leftNavigation = UIBarButtonItem
+    }
+    
+    func setupCollectionView() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memory.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width - 68, height: 505)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+}
