e5cf14db9e8b82f861bb334ce5bd9d16e7cbc6a1
Zedd0202 <ch41150@hanmail.net>
2019-08-17 17:28:27 +0900

    이미지 추가 및 address 추가.

---
+++ Plock/Scenes/MakePlace/MakePlaceContentView.swift
@@ -0,0 +1,21 @@
+//
+//  MakePlaceContentView.swift
+//  Plock
+//
+//  Created by Zedd on 17/08/2019.
+//  Copyright © 2019 Zedd. All rights reserved.
+//
+
+import UIKit
+
+class MakePlaceContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
